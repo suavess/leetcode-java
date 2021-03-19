8ececf0de825fddaa66d19e626af1e68a26230e7
Suave <1132888093@qq.com>
2021-03-19 09:43:38 +0800

    q21_合并两个有序链表

---
+++ src/com/suave/q21_合并两个有序链表/ListNode.java
@@ -0,0 +1,22 @@
+package com.suave.q21_合并两个有序链表;
+
+/**
+ * @author Suave
+ * @author 2021/3/16 10:48 上午
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/com/suave/q21_合并两个有序链表/Solution.java
@@ -0,0 +1,56 @@
+package com.suave.q21_合并两个有序链表;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+//
+//
+// 示例 2：
+//
+//
+//输入：l1 = [], l2 = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+// Related Topics 递归 链表
+// 👍 1598 👎 0
+
+/**
+ * @author Suave
+ * @author 2021/3/16 10:46 上午
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode res = l1.val < l2.val ? l1 : l2;
+        res.next = mergeTwoLists(res.next, l1.val >= l2.val ? l1 : l2);
+        return res;
+    }
+}
