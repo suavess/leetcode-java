657c067702b9cc8a61b408ba94370377c6779981
Suave <1132888093@qq.com>
2022-07-09 23:36:29 +0800

    feat:q1417_重新格式化字符串

---
+++ src/com/suave/q1417_重新格式化字符串/Solution.java
@@ -0,0 +1,99 @@
+package com.suave.q1417_重新格式化字符串;
+//给你一个混合了数字和字母的字符串 s，其中的字母均为小写英文字母。
+//
+// 请你将该字符串重新格式化，使得任意两个相邻字符的类型都不同。也就是说，字母后面应该跟着数字，而数字后面应该跟着字母。
+//
+// 请你返回 重新格式化后 的字符串；如果无法按要求重新格式化，则返回一个 空字符串 。
+//
+//
+//
+// 示例 1：
+//
+// 输入：s = "a0b1c2"
+//输出："0a1b2c"
+//解释："0a1b2c" 中任意两个相邻字符的类型都不同。 "a0b1c2", "0a1b2c", "0c2a1b" 也是满足题目要求的答案。
+//
+//
+// 示例 2：
+//
+// 输入：s = "leetcode"
+//输出：""
+//解释："leetcode" 中只有字母，所以无法满足重新格式化的条件。
+//
+//
+// 示例 3：
+//
+// 输入：s = "1229857369"
+//输出：""
+//解释："1229857369" 中只有数字，所以无法满足重新格式化的条件。
+//
+//
+// 示例 4：
+//
+// 输入：s = "covid2019"
+//输出："c2o0v1i9d"
+//
+//
+// 示例 5：
+//
+// 输入：s = "ab123"
+//输出："1a2b3"
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 500
+// s 仅由小写英文字母和/或数字组成。
+//
+//
+// Related Topics 字符串 👍 24 👎 0
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author suave
+ * @date 2022-07-09 23:01
+ */
+public class Solution {
+    public String reformat(String s) {
+        List<Character> nums = new ArrayList<>();
+        List<Character> strs = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 48 && s.charAt(i) <= 57) {
+                // 数字
+                nums.add(s.charAt(i));
+            } else {
+                strs.add(s.charAt(i));
+            }
+        }
+        if (nums.size() - strs.size() > 1 || nums.size() - strs.size() < -1) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        if (nums.size() > strs.size()) {
+            for (int i = 0; i < nums.size(); i++) {
+                result.append(nums.get(i));
+                if (i < strs.size()) {
+                    result.append(strs.get(i));
+                }
+            }
+        } else {
+            for (int i = 0; i < strs.size(); i++) {
+                result.append(strs.get(i));
+                if (i < nums.size()) {
+                    result.append(nums.get(i));
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().reformat("covid2019"));
+    }
+}
