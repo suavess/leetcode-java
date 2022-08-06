ae050806577ce0b97f6c2ab87ea49cb6d2002e82
Suave <1132888093@qq.com>
2022-08-06 23:57:53 +0800

    feat:q110_平衡二叉树

---
+++ src/com/suave/q110_平衡二叉树/Solution.java
@@ -0,0 +1,69 @@
+package com.suave.q110_平衡二叉树;
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
+//
+// 本题中，一棵高度平衡二叉树定义为：
+//
+//
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+//
+//
+// 示例 3：
+//
+//
+//输入：root = []
+//输出：true
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中的节点数在范围 [0, 5000] 内
+// -10⁴ <= Node.val <= 10⁴
+//
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1100 👎 0
+
+import com.suave.common.TreeNode;
+
+import java.util.Arrays;
+
+/**
+ * @author suave
+ * @date 2022-08-06 23:30
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) >= 0;
+    }
+
+    private int dfs(TreeNode node){
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = dfs(node.left);
+        int rightHeight = dfs(node.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
